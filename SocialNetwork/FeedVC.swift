18d47cd6454092220afd52dd5f1ae8678b6c7917
Sean Perez <sean.perez29@gmail.com>
2017-02-23 00:49:59 -0700

    Add Firebase GET and parsing

--- SocialNetwork/FeedVC.swift
+++ SocialNetwork/FeedVC.swift
@@ -13,12 +13,23 @@ import Firebase
 class FeedVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 81, left: 0, bottom: 0, right: 0)
         DataService.dataService.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value!)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? [String:AnyObject] {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
     }
     
@@ -35,12 +46,19 @@ class FeedVC: UIViewController {
 }
 
 extension FeedVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        let post = posts[indexPath.row]
+        print(post.caption)
         return cell
     }
 }
